fd97e0c33c253d6dd28bb1227350715b95f87886
Stephen McMahon <stephen.mcmahon@ucdconnect.ie>
2025-08-18 18:29:06 +0100

    Added additional comments to health test

--- backend/tests/test_health.py
+++ backend/tests/test_health.py
@@ -1,9 +1,10 @@
 from fastapi.testclient import TestClient
 from app.main import app
 
+# Declaring an instance of the test client class that is connected to the FastAPI app instance.
 client = TestClient(app)
 
 def test_health():
-    r = client.get("/health")
-    assert r.status_code == 200
-    assert r.json() == {"ok": True}
+    r = client.get("/health") # fake request sent to the health endpoint. JSON string is now contained in the r variable.
+    assert r.status_code == 200 # asserts that r's status code is 200.
+    assert r.json() == {"ok": True} # asserts that the json matches.
