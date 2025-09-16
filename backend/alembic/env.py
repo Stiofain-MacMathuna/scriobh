147cf474e5d3e38844bf8eaa0b1189783f18524f
Stephen McMahon <stephen.mcmahon@ucdconnect.ie>
2025-09-17 00:09:16 +0100

    chore: Updated github actions workflow and alembic env. Troubleshooting CI/CD issues

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -12,8 +12,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-DB_USER = os.getenv("DB_USER", "")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_HOST = os.getenv("DB_HOST", "")
 DB_PORT = int(os.getenv("DB_PORT", "5432"))
 DB_NAME = os.getenv("DB_NAME", "")
