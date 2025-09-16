a24c03fd9a68c0bedc889f0a9e75f0522a86d4f8
Stephen McMahon <stephen.mcmahon@ucdconnect.ie>
2025-09-16 22:35:15 +0100

    refractor: moved pydantic models relating to notes into separate modules in new schemas dir

---
+++ backend/app/api/schemas/notes.py
@@ -0,0 +1,21 @@
+# app/api/schemas/notes.py
+from pydantic import BaseModel
+from typing import Optional
+from uuid import UUID
+from datetime import datetime
+
+class NoteIn(BaseModel):
+    title: str
+    content: str
+
+class Note(BaseModel):
+    id: int
+    user_id: UUID
+    title: str
+    content: str
+    created_at: datetime
+    updated_at: datetime
+
+class NoteUpdate(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
