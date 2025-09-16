21a75dddddd19f204cae4b748eb297aa47613081
Stephen McMahon <stephen.mcmahon@ucdconnect.ie>
2025-09-16 22:48:51 +0100

    refractor: Add specific endpoint to verify the health of the db

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,56 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from .notes import router as notes_router
-from .db import init_db_pool, close_db_pool
+from .api.routes.notes import router as notes_router
+from .db import db_conn, init_db_pool, close_db_pool
+from .api.routes.auth import router as auth_router
+from fastapi.responses import JSONResponse
+import os
 
+print("DBG DB_HOST:", os.getenv("DB_HOST"))
+print("DBG DB_USER:", os.getenv("DB_USER"))
+print("DBG DB_NAME:", os.getenv("DB_NAME"))
+print("DBG DB_SSL_MODE:", os.getenv("DB_SSL_MODE"))
+
+# Lifespan shutdown / startup context manager. FastAPI instance calls this on start up and shutdown.
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db_pool()
     try:
         yield
     finally:
-        await close_db_pool
+        await close_db_pool()
 
-app = FastAPI()
+# Create fastapi instance and assign to app variable.
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 # health check route.
 @app.get("/health")
 async def health():
     return {"ok": True}
 
+# db health check.
+@app.get("/health/db")
+async def health_db():
+    # Fetch connection from the connection pool.
+    async with db_conn() as conn:
+        try:
+            val = await conn.fetchval("SELECT 1;") # use await so that if no connections are available the server can still continue other operations.
+
+            ok = (val == 1)
+        except Exception:
+            ok = False
+    code = status.HTTP_200_OK if ok else status.HTTP_503_SERVICE_UNAVAILABLE
+    return JSONResponse({"db_ok": ok}, status_code=code)
+
+# mount the auth_router and notes_router (which contain many routes) onto the main FastAPI application.
+app.include_router(auth_router) 
 app.include_router(notes_router, prefix="/notes", tags=["notes"])
